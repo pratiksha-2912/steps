17355ffb7aa7e7d18f749a2b2ecb196678436945
Pratiksha Shende <pratiksha.shende@ril.com>
2024-07-06 16:33:08 +0530

    steps

--- src/App-v1.js
+++ src/App-v1.js
@@ -9,7 +9,8 @@ const messages = [
 export default function App() {
   return (
     <div>
-      <Steps />
+     <Steps/>
+     <Steps/>
     </div>
   );
 }
@@ -42,25 +43,21 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              <span>⏮️</span>Previous
-            </Button>
-
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next<span>⏭️</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onCLick,children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onCLick}
-    >{children}</button>
   );
 }
